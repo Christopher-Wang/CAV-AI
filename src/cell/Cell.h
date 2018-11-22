103764f59b4e1b3628638e418b4c66e378c19544
Flavio B <redmannequin@users.noreply.github.com>
2018-11-21 21:00:35 -0500

    Create Cell.h

---
+++ src/cell/Cell.h
@@ -0,0 +1,19 @@
+#ifndef CELL_H
+#define CELL_H
+
+class Cell {
+	public:
+		Cell();
+		~Cell();
+    
+    // need to define common fucntion
+	
+	private:
+		int	n;
+    int m,
+    
+    // need to define a matrix
+    
+}
+
+#endif
