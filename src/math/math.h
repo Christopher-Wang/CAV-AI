bb621d1f19efc7c6c735ab98c1685503cdd27bfa
Flavio B <redmannequin@users.noreply.github.com>
2018-11-30 11:29:08 -0500

    Update math.h

--- src/math/math.h
+++ src/math/math.h
@@ -1,4 +1,38 @@
 #ifndef MATH_H
 #define MATH_H
 
+class Matrix {
+	public:
+		Matrix(int n, int m);
+		~Matrix();
+	
+		Matrix operator*(const int b);
+		Matrix operator*(const float b);
+		Matrix operator*(const Matrix& b);
+	
+		Matrix& operator*=(const int b);
+		Matrix& operator*=(const float b);
+		Matrix& operator*=(const Matrix& b);
+	
+		Matrix operator+(const int b);
+		Matrix operator+(const float b);
+		Matrix operator+(const Matrix& b);
+	
+		Matrix& operator+=(const int b);
+		Matrix& operator+=(const float b);
+		Matrix& operator+=(const Matrix& b);
+	
+		Matrix operator-(const int b);
+		Matrix operator-(const float b);
+		Matrix operator-(const Matrix& b);
+	
+		Matrix& operator-=(const int b);
+		Matrix& operator-=(const float b);
+		Matrix& operator-=(const Matrix& b);
+		
+	
+	protected:
+		float * data;
+};
+
 #endif
