beabe2e71c27b3011007d7f94f53caee6f204b2a
Flavio B <redmannequin@users.noreply.github.com>
2018-11-30 11:36:47 -0500

    Update Cell.h

--- src/cell/Cell.h
+++ src/cell/Cell.h
@@ -1,21 +1,26 @@
 #ifndef CELL_H
 #define CELL_H
 
+#include "../math/math.h"
+
 class Cell {
 	public:
 		Cell();
 		virtual ~Cell();
 	
 		void get_dim();
-		virtual void step() = 0;
-    
-    		// need to define common fucntion
+		virtual void step(Matrix a) = 0;
+	
+		// need to define common fucntion
 	
 	protected:
 		int n; // colums
-    		int m; // rows 
-    
-    		// need to define a matrix and other common vars
+    	int m; // rows
+	
+		Matrix weight;
+		Matrix bias;
+	
+		// need to define a matrix and other common vars
     
 }
 
