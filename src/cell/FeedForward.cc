e6746f9f4637c4e1e6f44b0f091e2ddcca2c8c47
Flavio B <redmannequin@users.noreply.github.com>
2018-11-30 11:43:40 -0500

    Create FeedForward.cc

---
+++ src/cell/FeedForward.cc
@@ -0,0 +1,8 @@
+#include "FeedForward.h"
+
+FeedForward:FeedForward (int n, int m) : Cell(n, m) {}
+FeedForward::~FeedForward(){}
+
+Matrix FeedForward::step(Matrix a) {
+	return (this->weight * a) + this->bias;
+}
